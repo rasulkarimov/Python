7bc711cde837c0e60175c30eea66680cd43c4580
rasulkarimov <rasul.karimov@gmail.com>
2021-01-09 20:14:49 +0300

    socket project

---
+++ iiec_rice/socket/recvfrom.py
@@ -0,0 +1,16 @@
+import socket
+
+# use ipv4 net address family
+afn = socket.AF_INET
+#use udp
+myp = socket.SOCK_DGRAM
+
+s = socket.socket( afn, myp )
+
+ip = "192.168.0.30"
+port = 1234
+
+s.bind( (ip,port) )
+
+x = s.recvfrom(1024)
+print(x)
