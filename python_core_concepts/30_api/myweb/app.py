a1de0849b3e316c736d6e92ce52dc5a882a42c1f
rasulkarimov <rasul.karimov@gmail.com>
2021-04-11 00:04:40 +0300

    api get post added

--- python_core_concepts/30_api/myweb/app.py
+++ python_core_concepts/30_api/myweb/app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask("myiiec")
 
-@app.route('/form')
-def myform():
-  return "form"
+# get form
+@app.route('/form_get')
+def myform_get():
+  return render_template("basic_get.html")
 
-@app.route('/data')
+# post form 
+@app.route('/form_post')
+def myform():
+  return render_template("basic_post.html")
+
+# dynamic route
+@app.route('/name/<y>')
+def myname(y):
+  return y
+
+@app.route('/data', methods=['POST', 'GET'])
 def mydata():
-  return "data"
+  if request.method == 'POST':
+    data = request.form.get('x')
+    print(data)
+  elif request.method == 'GET':
+    data = request.args.get('x')
+    print(data)
+  return data
 
 app.run(port=5555, debug=True)
